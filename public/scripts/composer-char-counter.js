74d3bff177a77aaaa218baa6e66e7eeb2b4ed9c0
Alexander Sidorenko <asiddev@protonmail.com>
2022-10-10 11:18:54 -0700

    Have charactersLeft and characters used being tracked. Next step is to show on screen

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,19 @@
 $("document").ready(function () {
-  console.log("ready");
+  console.log("working!");
+
+  $("#tweet-text").keyup(function (e) {
+    let charactersLeft = 144;
+    let counterUsed = 0;
+    if (counterUsed < charactersLeft) {
+      if (e.key === "Backspace") {
+        charactersLeft -= this.value.length;
+        counterUsed += this.value.length;
+      } else {
+        charactersLeft -= this.value.length;
+        counterUsed += this.value.length;
+      }
+    }
+    console.log("counterUsed", counterUsed);
+    console.log("charatersLeft", charactersLeft);
+  });
 });
