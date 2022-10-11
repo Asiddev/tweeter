62ccbc0211303115b021078c005e8701ff3613d4
Alexander Sidorenko <asiddev@protonmail.com>
2022-10-10 18:04:51 -0700

    using data object to render dynamic tweets using createTweetElement and renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,77 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$("document").ready(function () {
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+  const renderTweets = function (tweets) {
+    // loops through tweet
+    const container = $(".all-tweets-container");
+    for (let singleTweet of tweets) {
+      let tweet = createTweetElement(singleTweet);
+
+      // console.log("newTweet", tweet);
+
+      container.append(tweet);
+
+      // $(".all-tweets-container").append();
+    }
+
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+  };
+
+  const createTweetElement = function (tweet) {
+    let $tweet = $(`<div class="single-tweets-container">
+    <div class="tweets-container-content">
+      <div class="content-top">
+        <div class="content-top-right">
+        <img src="${tweet.user.avatars}"/>
+          <p>${tweet.user.name}</p>
+        </div>
+        <div class="content-top-left">
+          <p>${tweet.user.handle}</p>
+        </div>
+      </div>
+      <div class="content-middle">
+      ${tweet.content.text}
+      </div>
+      <div class="content-bottom">
+      ${new Date(tweet.created_at).toDateString()}
+        <div class="content-bottom-icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </div>
+    </div>
+  </div>`);
+
+    // ...
+    return $tweet;
+  };
+
+  renderTweets(data);
+});
